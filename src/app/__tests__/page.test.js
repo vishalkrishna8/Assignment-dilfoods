5af0b9017e85a4613861e20ed263dcdf071b0527
Vishal Krishna <121739011+vishalkrishna8@users.noreply.github.com>
2024-01-13 11:33:32 +0530

    Update: page.test.js

---
+++ src/app/__tests__/page.test.js
@@ -0,0 +1,15 @@
+// write test code here
+
+// import '@testing-library/jest-dom';
+// import { render, screen } from '@testing-library/react';
+// import Page from '../app/page';
+
+// describe('Page', () => {
+//   it('renders a paragraph', () => {
+//     render(<Page />);
+
+//     const paragraph = screen.getByRole('paragraph', { level: 1 });
+
+//     expect(paragraph).toBeInTheDocument();
+//   });
+// });
